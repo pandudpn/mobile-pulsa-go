aabfeb62455d53fe73de805ff0f6f11c1b735474
Pandu dwi Putra <pandu@pandudpn.id>
2022-04-11 18:10:30 +0700

    update constant and make global

---
+++ constant.go
@@ -0,0 +1,8 @@
+package mobilepulsa
+
+const (
+	BasePostpaidProduction  string = "https://mobilepulsa.net"
+	BasePrepaidProduction   string = "https://prepaid.iak.id"
+	BasePostpaidDevelopment string = "https://testpostpaid.mobilepulsa.net"
+	BasePrepaidDevelopment  string = "https://prepaid.iak.dev"
+)
